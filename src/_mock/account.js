e8d7bbd8d176baf0ed2b478d6bc3d1a03f532268
[StarIs] <[diablo8273@gmail.com]>
2024-03-10 21:08:40 +0900

    0310

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------
 
 export const account = {
-  displayName: 'Jaydon Frankie',
+  displayName: '슬림탄탄',
   email: 'demo@minimals.cc',
   photoURL: '/assets/images/avatars/avatar_25.jpg',
 };
